aace5a1f131264a03f31614584bc83b266e255db
unknown <doniramdani810@gmail.com>
2025-05-11 03:40:26 +0700

    Add .railwayignore and stage config changes

--- start.sh
+++ start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+# Install dependencies
+pip install -r requirements.txt
+
+# Install gdown for downloading models from Google Drive
 pip install gdown
 
+# Download model files from Google Drive
+# Model A
 gdown --id 1Km5IRryvsgCMTy9XCjZzC-fRlsDiIKZm --output best_model.pth
+# Model B
 gdown --id 1VdPhCp9d4RNLbnTjayNb3k4x-vIaxwi0 --output best_model1.pth
 
-gunicorn app:app --bind 0.0.0.0:$PORT
+# Run Flask app using Gunicorn
+exec gunicorn app:app --bind 0.0.0.0:$PORT
